a8871469fbbeee0b5fe31d0c2fff22833e1aee61
Muhammad Abdulllah <mabdullahzahid782@gmail.com>
2023-12-21 11:25:18 +0500

    Update

--- Class Grading System.asm
+++ Class Grading System.asm
@@ -1,68 +1,83 @@
 include emu8086.inc
 org 100h
 .data 
-    numberOfStudents DW 0                                                             
-	marksArray DB 50 DUP (?)           
-	idArray DB 50 DUP (?)
-	gradeArray DB 50 DUP (?)
-    marks DB 0
-	MSG1 DB 'Enter the number of students(DOES NOT EXCEED 50): ','$'                                 
-	MSG2 DB 'Enter the IDs of students: ','$'                     
-	MSG3 DB 0Dh,0Ah, 0Dh,0Ah,'Enter the marks of students: ','$'
-	MSG4 DB 0Dh,0Ah, 0Dh,0Ah,'ID: ',09H,'MARKS:',09H,'GRADE:','$'                                
-	HR DB '******************* Class Result ***********************','$'    
+    numberOfStudents dw 0                                                             
+	marksArray db 50 DUP (?)           
+	idArray db 50 DUP (?)
+	gradeArray db 50 DUP (?)
+    marks db 0
+	MSG1 db 'Enter the number of students(DOES NOT EXCEED 50): ','$'                                 
+	MSG2 db 'Enter the IDs of students: ','$'                     
+	MSG3 db 0dh,0ah, 0dh,0ah,'Enter the marks of students: ','$'
+	MSG4 db 0dh,0ah, 0dh,0ah,'ID: ',09h,'MARKS:',09h,'GRADE:','$'                                
+	HR db '******************* Class Result ***********************','$'    
 
 .code
 main proc
     mov ax, @data
     mov ds,ax
+    
+    start:
+    mov ax,3
+    int 10h
     mov dx,offset MSG1
     mov ah,9
     int 21h 
     call scan_num
-    mov numberOfStudents,cx 
-    
-    
-    
-    MOV SI, 0
+    mov numberOfStudents,cx
+    cmp numberOfStudents,50
+        jg errorBlock
+    cmp numberOfStudents,0
+        jl errorblock
+    jmp mainStart
+        
+    errorBlock:
+        mov ax,3
+        int 10h
+        print "Invalid Number of Students! Enter Valid Number of Students "
+        call scan_num 
+        mov cx,0
+        jmp start 
+    mainStart:
+    mov si, 0
     LOOP1:  
             mov ax, 3
             int 10h 
             mov dx, offset MSG2	
             mov ah,9
             int 21h
-            CALL SCAN_NUM 
-        	MOV idArray[SI],CL
+            call scan_num 
+        	mov idArray[si],cl
             mov dx, offset MSG3
             mov ah,9
             int 21h
             call scan_num
             mov marksArray[SI],CL
-        	INC SI  
-        	PRINT 0AH        
-        	PRINT 0DH         
-        	CMP SI,numberOfStudents 
-        	JNE LOOP1
+        	inc si  
+        	print 0ah        
+        	print 0dh         
+        	cmp si,numberOfStudents 
+        	jne LOOP1
     mov ax, 3
     int 10h
-    MOV DX, offset HR
-    MOV AH,9
-    INT 21h
-    MOV DX, offset MSG4
-    MOV AH,9
-    INT 21h 
-    PRINT 0AH 
-    PRINT 0DH  	
-    MOV SI, 0
+    mov dx, offset HR
+    mov ah,9
+    int 21h
+    mov dx, offset MSG4
+    mov ah,9
+    int 21h 
+    print 0ah 
+    print 0dh  	
+    mov si, 0
     LOOP3:  	
-            MOV AX,0
-        	MOV AL,idArray[SI]     
-        	CALL PRINT_NUM_UNS    
-        	PRINT 09H            
-        	MOV AL,marksArray[SI]
-        	CALL PRINT_NUM_UNS 
-        	PRINT 09H
-            MOV marks,AL 
+            mov ax,0
+        	mov al,idArray[si]     
+        	call print_num_uns    
+        	print 09h            
+        	mov al,marksArray[si]
+        	call print_num_uns 
+        	print 09h
+            mov marks,al 
             
             cmp marks,100
             jg error
@@ -84,14 +99,13 @@ main proc
             jge eighth  
             cmp marks,50
             jge ninth 
-            cmp marks,50
-            jge ninth
             cmp marks,40
-            jge fail
+            jge tenth
+            cmp marks,40
+            jl fail
             cmp marks,0
             jl error
-            
-            
+           
             first:
                 print 'A+'
                 jmp thread  
@@ -127,16 +141,49 @@ main proc
                 jmp thread
             error:
                 print 'Invalid Marks' 
-                jmp thread
+                jmp thread  
+                
             thread:
-        	PRINT 0AH             
-        	PRINT 0DH            
-        	INC SI 
-        	CMP SI,numberOfStudents 
-        	JNE LOOP3
-    
-    
-     
+        	print 0ah             
+        	print 0dh            
+        	inc si 
+        	cmp si,numberOfStudents 
+        	jne LOOP3 
+        	call scan_num
+        	mov cx,0
+        	option:
+        	mov ax,3
+        	int 10h
+        	print "Try Another: "
+        	print 0ah
+        	print 0dh
+        	print "1) Yes" 
+        	print 0ah
+        	print 0dh
+        	print "2) No" 
+        	print 0ah
+        	print 0dh
+        	print "Select Option: "
+        	call scan_num
+        	cmp cl,1
+        	    je start
+        	cmp cl,2
+        	    jne errorOption
+        	jmp endOfProgram
+        	errorOption: 
+        	    mov ax,3
+        	    int 10h
+        	    print "Invalid Option! Select Valid Option "
+        	    call scan_num
+        	    mov cx,0
+        	    jmp option 
+        	
+        	endOfProgram:
+        	    mov ax,3
+        	    int 10h
+        	    print "Thanks for using me ;)"
+        	    
+        	
 DEFINE_SCAN_NUM            
 DEFINE_PRINT_NUM
 DEFINE_PRINT_NUM_UNS
